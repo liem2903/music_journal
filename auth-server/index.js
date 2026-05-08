ffb81dddf56ed7a31d2ed584c32bd4b00ce0c170
Liem Phan <z5419546@student.unsw.edu.au>
2026-05-08 16:31:08 +1000

    chore: load balancer created.

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors'
+
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.listen(process.env.PORT, () => {console.log(`Server is listening on port ${process.env.PORT_A}`)});
 
---
+++ load-balancer/index.js
@@ -0,0 +1,19 @@
+import httpProxy from 'http-proxy';
+import http from 'http';
+import 'dotenv/config';
+
+const proxy = httpProxy.createProxyServer()
+
+const servers = [
+  'http://auth-server-1:3001',
+  'http://auth-server-2:3002',
+  'http://auth-server-3:3003'
+]
+
+let current = 0
+
+http.createServer((req, res) => {
+  const target = servers[current % servers.length]
+  current++
+  proxy.web(req, res, { target })
+}).listen(process.env.PORT)
